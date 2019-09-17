a0f6b6e7d60e81987d7d37b0b2e142b71121b9a6
Sergey <ico1161441@gmail.com>
2019-09-17 14:13:38 +0200

    Added PHPUnit

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInit4d60de4b55dbf2af47c0ca604700d464
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit4d60de4b55dbf2af47c0ca604700d464::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire4d60de4b55dbf2af47c0ca604700d464($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire4d60de4b55dbf2af47c0ca604700d464($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
