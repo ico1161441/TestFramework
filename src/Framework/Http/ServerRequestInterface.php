9906f52fbb3ec7efa855e77f9c70c48f7a34f1a7
Sergey <ico1161441@gmail.com>
2019-09-17 16:59:23 +0200

    Added interfaces

---
+++ src/Framework/Http/ServerRequestInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Framework\Http;
+
+interface ServerRequestInterface
+{
+    public function getQueryParams(): array;
+
+    public function withQueryParams(array $query);
+
+    public function getParsedBody();
+
+    public function withParsedBody($data);
+}
