f3bbaff2e9d8e19b72f5991a770a111c102e78f6
Sergey <ico1161441@gmail.com>
2019-09-17 11:53:21 +0200

    Created composer.json

--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Hello!";
+use Framework\Http\Request;
+
+$name = $_GET['name'] ?? 'Guest';
+
+chdir(dirname(__DIR__));
+require_once "src/Framework/Http/Request.php";
+
+
+
+$request = new Request();
+
+$name = $request->getQueryParams()['name'] ?? 'Guest';
+echo "Hello, $name!";
