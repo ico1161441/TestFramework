eafa0df513b4c3a0cc2a5b51993c044455a166f6
Sergey <ico1161441@gmail.com>
2019-09-17 16:16:41 +0200

    Added PHPUnit ResponseTest

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Framework\Http\RequestFactory;
+use Framework\Http\Response;
 
 chdir(dirname(__DIR__));
 require_once "vendor/autoload.php";
@@ -8,4 +9,11 @@ require_once "vendor/autoload.php";
 $request = RequestFactory::fromGlobals();
 
 $name = $request->getQueryParams()['name'] ?? 'Guest';
-echo "Hello, $name!";
+
+$response = (new Response('Hello, ' . $name . '!'))->withHeader('X-Dev', 'ElisDN');
+
+header('HTTP/1.0' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
+foreach ($response->getHeaders() as $name => $value) {
+    header($name . ':' . $value);
+}
+echo $response->getBody();
